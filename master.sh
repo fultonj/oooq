ce03a50467c8d8000860838d9a51894483dcf276
John Fulton <fulton@redhat.com>
2017-01-11 13:31:56 -0500

    added nasty hack to master.sh to update my .emacs

--- master.sh
+++ master.sh
@@ -2,13 +2,14 @@
 # Filename:                master.sh
 # Description:             Sets up my dev env
 # Supported Langauge(s):   GNU Bash 4.3.x
-# Time-stamp:              <2017-01-11 10:34:01 jfulton> 
+# Time-stamp:              <2017-01-11 13:29:58 jfulton> 
 # -------------------------------------------------------
-CLONEQ=1
-RUNQ=1
-DISK=1
-IMG=1
-SCRIPTS=1
+CLONEQ=0
+RUNQ=0
+DISK=0
+IMG=0
+SCRIPTS=0
+LOCAL=1
 # -------------------------------------------------------
 export SSH_ENV=~/.quickstart/ssh.config.ansible
 export VIRTHOST=$(hostname)
@@ -36,4 +37,14 @@ if [ $SCRIPTS -eq 1 ]; then
     scp -F $SSH_ENV scripts.tar.gz stack@undercloud:/home/stack/
     ssh -F $SSH_ENV stack@undercloud "tar xf scripts.tar.gz"
     ssh -F $SSH_ENV stack@undercloud "echo 'source /home/stack/stackrc' >> ~/.bashrc"
+fi
+
+if [ $LOCAL -eq 1 ]; then
+    # install my personal key on undercloud using key provided by quickstart
+    scp -F $SSH_ENV cat ~/.ssh/id_rsa.pub stack@undercloud:/home/stack/
+    ssh -F $SSH_ENV stack@undercloud "cat ~/id_rsa.pub >> ~/.ssh/authorized_keys"
+    # get local simple IP that my emacs tramp can reach
+    ip=$(ssh -F $SSH_ENV stack@undercloud "/sbin/ip a | grep 192.168.23" | awk {'print $2'} | sed s/\\/24//g)
+    # update my tramp entry (assuming my .emacs only talks to 23 for underlap)
+    sed -i -e s/192.168.23.[0-9]*/$ip/g ~/.emacs
 fi

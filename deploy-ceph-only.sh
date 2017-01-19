44c6da8181cf66a6c0a5e51f293111a8d116597e
John Fulton <fulton@redhat.com>
2017-01-19 03:15:32 +0000

    update deploy-ceph-only.sh to use the new ~/templates symlink to the git repo and not /usr/share

--- deploy-ceph-only.sh
+++ deploy-ceph-only.sh
@@ -1,5 +1,5 @@
 source ~/stackrc
 pushd ~ ; upload-puppet-modules -d puppet-modules ; popd
-time openstack overcloud deploy --templates \
+time openstack overcloud deploy --templates ~/templates \
 --compute-scale 0 --ceph-storage-scale 1 \
 -e ~/tht/ceph-only.yaml

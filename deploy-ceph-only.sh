eb60c58ac1d5a84630f3e904279ecaab7d399a83
John Fulton <fulton@redhat.com>
2017-01-19 22:34:31 +0000

    update deploy scripts to use --libvirt-type qemu

--- deploy-ceph-only.sh
+++ deploy-ceph-only.sh
@@ -1,5 +1,6 @@
 source ~/stackrc
 pushd ~ ; upload-puppet-modules -d puppet-modules ; popd
 time openstack overcloud deploy --templates ~/templates \
+--libvirt-type qemu \
 --compute-scale 0 --ceph-storage-scale 1 \
 -e ~/tht/ceph-only.yaml
--- deploy-hci.sh
+++ deploy-hci.sh
@@ -1,6 +1,7 @@
 source ~/stackrc
 pushd ~ ; upload-puppet-modules -d puppet-modules ; popd
 time openstack overcloud deploy --templates ~/templates \
+--libvirt-type qemu \
 -r ~/tht/custom-roles.yaml \
 -e /home/stack/templates/environments/storage-environment.yaml \
 -e ~/tht/ceph.yaml \

8ca53187f4f8b1c09168bfb8db46cdbc9ddc529b
John Fulton <fulton@redhat.com>
2017-02-03 17:39:59 -0500

    update vms.sh to support starting/stopping undercloud

--- vms.sh
+++ vms.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env bash
 # Filename:                vms.sh
 # Description:             runs commands as stack@virthost
-# Time-stamp:              <2017-01-16 08:57:43 jfulton> 
+# Time-stamp:              <2017-02-03 17:30:26 jfulton> 
 # -------------------------------------------------------
 # another crude but effective wrapper for the lazy
 # -------------------------------------------------------
 case "$1" in
+        undercloud)
+	    case "$2" in
+		stop)
+		    cmd='virsh stop undercloud'
+		    ;;
+		start)
+		    cmd='virsh start undercloud'
+		    ;;
+		*)
+		    cmd="virsh list --all"
+	    esac
+            ;;
         resume)
 	    cmd='for vm in $(virsh list --all | awk {"print \$2"} | egrep -v "Name|^$"); do echo "running resume $vm"; virsh resume $vm; done'
             ;;

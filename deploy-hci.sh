817b8a5bd778549021ccc560ec7b9fac5c0d6436
John Fulton <fulton@redhat.com>
2017-02-09 19:26:55 +0000

    Update deploy-hci and deploy-jeos scripts to separate layouts
    - deploy-hci.sh now includes hci-layout.yaml
    - deploy-jeos.sh now includes jeos-layout.yaml
    - the generic layout.yaml is gone
    - rather than edit layout.yaml dependending on the deployment
      have dedicated versions per deployment type

--- deploy-hci.sh
+++ deploy-hci.sh
@@ -5,4 +5,4 @@ time openstack overcloud deploy --templates ~/templates \
 -r ~/tht/custom-roles.yaml \
 -e /home/stack/templates/environments/storage-environment.yaml \
 -e ~/tht/ceph.yaml \
--e ~/tht/layout.yaml
+-e ~/tht/hci-layout.yaml
--- deploy-jeos.sh
+++ deploy-jeos.sh
@@ -3,4 +3,4 @@ pushd ~ ; upload-puppet-modules -d puppet-modules ; popd
 time openstack overcloud deploy --templates ~/templates \
 --libvirt-type qemu \
 -r ~/tht/custom-roles.yaml \
--e ~/tht/layout.yaml
+-e ~/tht/jeos-layout.yaml

4663c1102d54719891376af1444c84f598f61b82
John Fulton <fulton@redhat.com>
2017-02-11 22:18:52 +0000

    update deploy-mistral-ceph-hci.sh to include tht/custom-roles.yaml to use new light-weight controller role

--- deploy-mistral-ceph-hci.sh
+++ deploy-mistral-ceph-hci.sh
@@ -2,6 +2,7 @@ source ~/stackrc
 pushd ~ ; upload-puppet-modules -d puppet-modules ; popd
 time openstack overcloud deploy --templates ~/templates \
 --libvirt-type qemu \
+-r ~/tht/custom-roles.yaml \
 -e /home/stack/templates/environments/puppet-ceph-external.yaml \
 -e ~/tht/external-ceph.yaml \
 -e ~/tht/mistral-ceph-layout.yaml

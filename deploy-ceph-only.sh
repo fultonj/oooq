8e04565ad9d21d47f23d650a6f7361bb766e7314
John Fulton <fulton@redhat.com>
2017-09-01 11:51:21 +0000

    update deploy-ceph-only.sh to name stack ceph so as to not conflict with other stack named overcloud

--- deploy-ceph-only.sh
+++ deploy-ceph-only.sh
@@ -1,6 +1,6 @@
 source ~/stackrc
 pushd ~ ; upload-puppet-modules -d puppet-modules ; popd
 time openstack overcloud deploy --templates ~/templates \
+--stack ceph \
 --libvirt-type qemu \
---compute-scale 0 --ceph-storage-scale 1 \
 -e ~/tht/ceph-only.yaml

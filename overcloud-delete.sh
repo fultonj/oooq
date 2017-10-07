4ac02e4f31461a12bc53f52cee537ecf3c82d9e0
John Fulton <fulton@redhat.com>
2017-10-07 12:20:03 -0400

    updating overcloud-delete.sh to re-cleaning in the event of node clean failed status

--- overcloud-delete.sh
+++ overcloud-delete.sh
@@ -12,3 +12,27 @@ fi
 #echo "Deleting Swift Container of overcloud Heat templates"
 #openstack overcloud plan delete overcloud 
 # fixme: the above shouldn't be necessary as the plan should be deleted
+
+echo "checking on node cleaning"
+openstack baremetal node list | grep clean 
+
+echo "The following node(s) have clean failed status"
+echo "Following https://docs.openstack.org/ironic/latest/admin/cleaning.html"
+
+for node_ident in $(openstack baremetal node list | grep "clean failed" | awk {'print $2'});
+do
+    echo "moving $node_ident to manageable state"
+    openstack baremetal node manage $node_ident
+
+    echo "moving $node_ident out of maintenance mode"    
+    openstack baremetal node maintenance unset $node_ident
+
+    echo "marking $node_ident as available for scheduling by nova"
+    openstack baremetal node provide $node_ident
+    
+    echo "node $node_ident should now attempt cleaning again"
+    openstack baremetal node list | grep clean | grep $node_ident
+done
+
+echo "Re-checking on node cleaning"
+openstack baremetal node list | grep clean 

5b7de4d1dc8c5e40179e054ae9afe08f1933d6dc
John Fulton <fulton@redhat.com>
2018-01-23 19:01:29 -0500

    update poll.sh to list contents of pending heat config jobs

--- deployed/poll.sh
+++ deployed/poll.sh
@@ -4,3 +4,6 @@ source ~/stackrc
 export OVERCLOUD_ROLES="Controller"
 export Controller_hosts="192.168.2.2"
 ~/templates/deployed-server/scripts/get-occ-config.sh
+
+OVER=$Controller_hosts
+ssh $OVER -l stack "sudo ls -lhtr /var/lib/heat-config/heat-config-script/"

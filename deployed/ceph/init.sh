a2f951ac057c7011e9d607c4fb7e412ba0eb09e7
John Fulton <fulton@redhat.com>
2018-02-02 17:02:11 -0500

    update deployed/ceph/init.sh to cp site-docker.yaml (not site.yaml)

--- deployed/ceph/init.sh
+++ deployed/ceph/init.sh
@@ -5,7 +5,7 @@ export ANSIBLE_NOCOWS=1
 git clone https://github.com/ceph/ceph-ansible.git
 
 cp group_vars/* ceph-ansible/group_vars/
-cp ceph-ansible/site.yml.sample ceph-ansible/site.yml
+cp ceph-ansible/site-docker.yml.sample ceph-ansible/site-docker.yml
 
 echo "Is inventory good?"
 ansible mgrs -m ping

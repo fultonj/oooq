04ca430ff0999f5c7695a223b9ea79e780e1f56a
John Fulton <fulton@redhat.com>
2018-02-22 23:33:34 +0000

    Update deploy.sh to break deployment into three phases
    deploy.sh now produces the same result (modulo low-mem-usage and
    disable-telemetry) except it does the deployment in three phases:
    
    HEAT: bare metal deployment and make heat stack of config data
    CONF: download playbooks and verify ansible ping in nicely named dir
    PLAY: run playbooks to finish overcloud deployment.
    
    Though this took more effort than just keeping --config-download in
    the openstack overcloud deploy command, I can now break the deploy
    up into these pieces and repeat only the parts that interest me and
    do the majority of the configuration in ansible.

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,9 +1,48 @@
 #!/usr/bin/env bash
 
+HEAT=1
+CONF=1
+PLAY=1
+
 source ~/stackrc
 
-time openstack overcloud deploy \
-    --templates ~/templates/ \
-    --config-download \
-    -e ~/templates/environments/config-download-environment.yaml \
-    -e ./overrides.yaml
+if [[ $HEAT -eq 1 ]]; then
+    time openstack overcloud deploy \
+	 --templates ~/templates/ \
+	 -e ~/templates/environments/low-memory-usage.yaml \
+	 -e ~/templates/environments/disable-telemetry.yaml \
+	 -e ~/templates/environments/config-download-environment.yaml \
+	 -e ./overrides.yaml
+    
+    # Add the following to the above to make CONF/PLAY unnecessary
+    #   --config-download 
+fi
+# -------------------------------------------------------
+if [ -z "$1" ]; then
+    name=$(date +%a-%I%M%p)    
+else
+    name=$1
+fi
+# -------------------------------------------------------
+if [[ $CONF -eq 1 ]]; then
+    tripleo-config-download  
+    if [[ ! -d tripleo-config-download ]]; then
+	echo "tripleo-config-download cmd didn't create tripleo-config-download dir"    
+    else
+	target=tripleo-config-download/$(ls -tr tripleo-config-download | tail -1)
+	ln -s $target NAME
+	tripleo-ansible-inventory --static-yaml-inventory $name/inventory.yaml
+	ansible -i $name/inventory.yaml all -m ping
+	echo "pushd $name"
+	echo 'ansible -i inventory.yaml all -m shell -b -a "hostname"'
+    fi
+fi
+# -------------------------------------------------------
+if [[ $PLAY -eq 1 ]]; then
+    time ansible-playbook \
+	 -v \
+	 --become \
+	 --ssh-extra-args "-o StrictHostKeyChecking=no" --timeout 240 \
+	 -i $name/inventory.yaml \
+	 $name/deploy_steps_playbook.yaml
+fi

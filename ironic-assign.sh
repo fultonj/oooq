6478f2f6af2e650aa214b2b63c65300beca42694
John Fulton <fulton@redhat.com>
2018-02-28 17:32:42 +0000

    update ironic-assign to take opt for node vs profile

--- ironic-assign.sh
+++ ironic-assign.sh
@@ -1,16 +1,26 @@
 # Filename:                ironic-assign.sh
 # Description:             Assign ironic nodes to $name
 # Supported Langauge(s):   GNU Bash 4.2.x
-# Time-stamp:              <2016-11-23 19:34:25 jfulton> 
+# Time-stamp:              <2017-11-10 07:48:10 fultonj> 
 # -------------------------------------------------------
 if [[ $# -eq 0 ]] ; then
-    echo "usage: $0 <REGEX> <NAME>"
+    echo "usage: $0 <REGEX> <NAME> <OPT>"
     echo "  <REGEX>   regex matching the nodes to be tagged; e.g. \"730\" or \"630\""
     echo "  <NAME>    name desired for node; e.g. \"osd-compute\" or \"controller\""
+    echo "  <OPT>     opt=1 -> node (default) | opt=2 -> profile | opt=3 -> node+profile"
     exit 1
 fi
 regex=$1
 name=$2
+var=$3
+
+if [[ ${var:+1} ]] ; then
+    echo "OPT is $var"
+    opt=$var
+else
+    echo "Defaulting OPT to 1"
+    opt=1
+fi
 
 source ~/stackrc
 echo "Assigning nodes from ironic's list that match $regex"
@@ -28,7 +38,15 @@ echo "$count nodes match $regex"
 i=0
 for id in $(cat /tmp/n_nodes); do
     node="$name-$i"
-    ironic node-update $id replace properties/capabilities=node:$node,boot_option:local
+    if [[ $opt -eq 1 ]]; then
+	ironic node-update $id replace properties/capabilities=node:$node,boot_option:local
+    fi
+    if [[ $opt -eq 2 ]]; then
+	ironic node-update $id replace properties/capabilities=profile:$name,boot_option:local
+    fi
+    if [[ $opt -eq 3 ]]; then
+	ironic node-update $id replace properties/capabilities=profile:$name,node:$node,boot_option:local
+    fi
     i=$(expr $i + 1)
 done
 
@@ -36,6 +54,6 @@ echo "Ironic node properties have been set to the following:"
 for ironic_id in $(ironic node-list | awk {'print $2'} | grep -v UUID | egrep -v '^$');
 do
     echo $ironic_id;
-    ironic node-show $ironic_id  | egrep -A 1 "memory_mb|profile|wwn" ;
+    ironic node-show $ironic_id  | egrep -A 2 "memory_mb|profile|wwn" ;
     echo "";
 done

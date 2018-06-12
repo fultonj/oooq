7a9991917e0a16bd0aaa5cefa55356dfb565a4e1
John Fulton <fulton@redhat.com>
2018-06-12 15:05:34 -0400

    delete five-month-old scripts

--- old/deploy-jeos.sh
+++
@@ -1,6 +0,0 @@
-source ~/stackrc
-pushd ~ ; upload-puppet-modules -d puppet-modules ; popd
-time openstack overcloud deploy --templates ~/templates \
---libvirt-type qemu \
--r ~/tht/custom-roles.yaml \
--e ~/tht/jeos-layout.yaml

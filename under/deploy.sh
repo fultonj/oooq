04971eb555aa3a8cc2425eb190be07d26992f7f3
John Fulton <fulton@redhat.com>
2018-06-12 17:48:40 -0400

    update overrides.yaml oooq_ flavors

--- under/deploy.sh
+++ under/deploy.sh
@@ -73,6 +73,8 @@ fi
 if [ $SCRIPTS -eq 1 ]; then
     ssh -F $SSH_ENV stack@undercloud "echo 'ssh-keyscan github.com >> ~/.ssh/known_hosts' > sh_me"
     ssh -F $SSH_ENV stack@undercloud "echo 'git clone git@github.com:fultonj/oooq.git' >> sh_me"
+    ssh -F $SSH_ENV stack@undercloud "echo 'ln -s ~/oooq/over/deploy.sh >> sh_me"
+    ssh -F $SSH_ENV stack@undercloud "echo 'ln -s ~/oooq/over/overrides.yaml >> sh_me"
     ssh -F $SSH_ENV stack@undercloud "echo 'source /home/stack/stackrc' >> ~/.bashrc"
     ssh -F $SSH_ENV stack@undercloud "echo 'alias os=openstack' >> ~/.bashrc"    
 fi

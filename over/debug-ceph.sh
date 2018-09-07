8f466386909b3e4e9223ce1bd6fa3492fe1a9ae5
John Fulton <fulton@redhat.com>
2018-09-07 11:39:32 +0000

    Add debug-ceph.sh
    Just a wrapper to speed up this process
    
    https://docs.openstack.org/tripleo-docs/latest/install/advanced_deployment/ansible_config_download.html#tags

--- over/debug-ceph.sh
+++ over/debug-ceph.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# speed up this process:
+# https://docs.openstack.org/tripleo-docs/latest/install/advanced_deployment/ansible_config_download.html#tags
 
 source ~/stackrc
 
@@ -11,4 +13,7 @@ fi
 
 sudo setfacl -R -m u:$USER:rwx /var/lib/mistral
 pushd $DIR
+ls -l external_deploy_steps_tasks.yaml
 bash ansible-playbook-command.sh --tags external_deploy_steps
+
+

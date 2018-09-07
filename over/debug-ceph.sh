31335f64cc269f4558efb35164375198cf30c7c0
John Fulton <fulton@redhat.com>
2018-09-07 12:17:43 +0000

    add clean option to debug-ceph.sh

--- over/debug-ceph.sh
+++ over/debug-ceph.sh
@@ -3,7 +3,6 @@
 # https://docs.openstack.org/tripleo-docs/latest/install/advanced_deployment/ansible_config_download.html#tags
 
 source ~/stackrc
-
 DIR=/var/lib/mistral/overcloud
 
 if [[ ! -e $DIR ]]; then
@@ -11,9 +10,16 @@ if [[ ! -e $DIR ]]; then
     exit 1
 fi
 
-sudo setfacl -R -m u:$USER:rwx /var/lib/mistral
-pushd $DIR
-ls -l external_deploy_steps_tasks.yaml
-bash ansible-playbook-command.sh --tags external_deploy_steps
+if [ -n "$1" ]; then 
+    if [[ $1 == "clean" ]]; then
+	echo "Deleting all files in $DIR owned by $USER so mistral will not have permissions issues"
+	find $DIR -user $USER -exec rm -rf {} \;
+    fi
+ else
+    sudo setfacl -R -m u:$USER:rwx /var/lib/mistral
+    ls -l external_deploy_steps_tasks.yaml
+    bash ansible-playbook-command.sh --tags external_deploy_steps
+fi
+
 
 
